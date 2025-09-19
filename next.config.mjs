b870f59dc36616b9528d0300868195483f8369e5
quq-opa <gamerofpixelgund@gmail.com>
2025-09-20 01:14:33 +0200

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  // говорим Next.js что код лежит в src/
   eslint: {
     ignoreDuringBuilds: true,
   },
